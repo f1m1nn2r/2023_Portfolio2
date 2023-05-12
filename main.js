511a58c67f0197e9f6c853115cd621920fe529a6
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-05-12 20:08:09 +0900

    2023.05.12_20:08

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-/****** 섹션03 ******/ 
+// 부드러운 스크롤 효과, 출처 https://creativestudio.kr/2113
+$("html").easeScroll();
+$("html").easeScroll({
+    frameRate: 60,
+    animationTime: 1000,
+    stepSize: 100,
+    pulseAlgorithm: !0,
+    pulseScale: 15,
+    pulseNormalize: 1,
+    accelerationDelta: 20,
+    accelerationMax: 1,
+    keyboardSupport: !0,
+    arrowScroll: 50
+});
+
 const contentColumn_li = document.querySelectorAll('.section03 .content-column ul li');
 var contentColumn_img = new Array();
 for(var i=0; i<contentColumn_li.length; i++){
@@ -35,6 +49,7 @@ for(var i=0; i<contentColumn_li.length; i++){
         }
     });
 
+    // 클릭 시 특정 위치로 이동
     contentColumn_li[i].addEventListener('click', function(){
         const link = document.querySelector('.sub_link');
         var linkLocation = window.pageYOffset + link.getBoundingClientRect().top;
@@ -45,25 +60,51 @@ for(var i=0; i<contentColumn_li.length; i++){
     });
 }
 
+// 마우스 움직임에 맞춰 이미지 이동되도록
 window.addEventListener('mousemove', function(e){
-    var posX = e.clientX / 50;
-    var posY = e.clientY / 50;
+    var posX = e.clientX / 120;
+    var posY = e.clientY / 120;
     const workImg = document.querySelectorAll('.work-img');
     for(var i=0; i<workImg.length; i++){
         workImg[i].style.transform = `translate3d(${posX}vw, ${posY}vw, 0)`;
     }
 });
 
-// 스크롤 시 텍스트 opacity 조정
 window.addEventListener('scroll', function(){
+    // 스크롤 시 첫 번째 섹션 y값 조정
+    const firstSection = document.querySelector('.section01-inner');
+    const firstIntro = document.querySelector('.section01 .intro-txt-wrap');
+    const firstTxt = document.querySelector('.section01 .m-txt-wrap');
+    var yValue = window.pageYOffset * 6 / firstIntro.offsetTop;
+    firstSection.style.transform = `translate3d(0, ${yValue}vw, 0)`;
+    // firstSection과 달리 올라가지 않고 밑에서 겹쳐지는 현상 보여 따로 추가
+    firstTxt.style.transform = `translate3d(0, ${-yValue}vw, 0)`;
+
+    // 스크롤 시 텍스트 opacity 조정
     var opacityValue = 0.7 + (window.scrollY / document.documentElement.scrollHeight);
     for(var i=0; i<contentColumn_li.length; i++){
         contentColumn_li[i].firstChild.style.opacity = '0.3';
-        // 화면 스크롤이 중앙 지점에 도착할 때 기준
+        // 화면 스크롤이 중앙 지점일 때의 기준
         if(contentColumn_li[i].getBoundingClientRect().top < window.innerHeight / 2){
             contentColumn_li[i].firstChild.style.opacity = opacityValue;
         }else{
             contentColumn_li[i].firstChild.style.opacity = '0.3';
+        }
+    }
+
+    // 스크롤 시 텍스트 좌 우 이동 애니메이션
+    const RLScroll_section = document.querySelectorAll('.txt-scroll-section');
+    const RLScroll_txt = document.querySelectorAll('.rl-scroll');
+    var RLScrollValue = window.pageYOffset / document.querySelector('.section01 .intro-txt-wrap').offsetTop;
+    for(var i=0; i<RLScroll_section.length; i++){
+        for(var j=0; j<RLScroll_txt.length; j++){
+            if(window.scrollY >= RLScroll_section[i].offsetTop * 1.3){
+                if(j % 2 == 0){
+                    RLScroll_txt[j].style.transform = `translate3d(${RLScrollValue}vw, 0, 0)`
+                }else if(j % 2 == 1){
+                    RLScroll_txt[j].style.transform = `translate3d(${-RLScrollValue}vw, 0, 0)`
+                }
+            }
         }
     }
 });
