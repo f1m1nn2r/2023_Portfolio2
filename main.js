e1901b58b7df0e5ee794b27b267b8959df63d2f9
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-05-12 14:57:21 +0900

    2023.05.12_14:57

---
+++ main.js
@@ -0,0 +1,8 @@
+// 섹션01 라인 width 값 
+var sc01_p = document.querySelectorAll('.bot-right p');
+var lineDashed = document.querySelectorAll('.bot-right .line_dash'); 
+
+for(var i=0; i<sc01_p.length; i++){
+    var sc01_p_width = sc01_p[i].clientWidth;
+    lineDashed[i].style.width = `calc(100% - ${sc01_p_width}px)`;
+}
